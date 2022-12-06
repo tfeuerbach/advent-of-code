54c2e0a3ae5c89a0f783ac82a2eaeacf81eecbae
Thomas Feuerbach <thfeuerbach@gmail.com>
2022-12-06 15:23:50 -0500

    day 5 work-in-progress

--- 2022/day-05/solution.py
+++ 2022/day-05/solution.py
@@ -13,24 +13,35 @@ table = {'1': ['D', 'T', 'R', 'B', 'J', 'L', 'W', 'G'],
          '8': ['L', 'H', 'R', 'B', 'T', 'V', 'M'],
          '9': ['Q', 'P', 'D', 'S', 'V']}
 
+# test_table = {'1': ['Z', 'N'], '2': ['M', 'C', 'D'], '3': ['P']}
+
+
 def crane_do(instruction):
     quantity = instruction[0]
     source = instruction[1]
     destination = instruction[2]
-    for key, value in table.items():
-        moved_crates = np.flip(table[str(source)][-quantity:])
-        for crate in moved_crates:
-            table[str(source)].remove(crate)
-            table[str(destination)].append(crate)
-        return table
-
+    print('move '+str(quantity)+' from '+str(source)+' to '+str(destination))
+    moved_crates = np.flip(table[str(source)][-quantity:])
+    print('MOVED CRATES'+str(moved_crates))
+    print('BEFORE'+str(table))
+    for crate in moved_crates:
+        table[str(source)].remove(crate)
+        table[str(destination)].append(crate)
+    print('AFTER'+str(table)+'\n')
+    return table
+    
 def cargo_sort(filename):
-    instructions = read_and_split(filename)
+    instructions = read_and_split(filename) # instructions = file.read().splitlines()
+    top_crates = []
     for step in instructions:
-        print(step)
         parsed_step = [int(i) for i in step.split() if i.isdigit()]
-        crane_do(parsed_step)
-    print(table)
+        table = crane_do(parsed_step)
+    for stack, crates in table.items():
+        top_crate = list(crates[-1])
+        top_crates.extend(top_crate)
+    print(top_crates)
+    
+
 if len(sys.argv) == 2:
     cargo_sort(sys.argv[1])
 else:
---
+++ 2022/lib/read_and_split.py
@@ -0,0 +1,2 @@
+def read_and_split(filename):
+    return open(filename).read().splitlines()
