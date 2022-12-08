372aa532afe6de013ce8a4c4a52040b8a5f04a73
Thomas Feuerbach <thfeuerbach@gmail.com>
2022-12-07 23:15:59 -0500

    completed day 7

--- 2022/day-07/solution.py
+++ 2022/day-07/solution.py
@@ -4,7 +4,8 @@ from lib.read_and_split import *
 from collections import defaultdict
 from itertools import accumulate
 
-# Part 1 and 2 -- 
+# Part 1 and 2 -- Find the total sum of directories that are at or below 100,000 bytes.
+#                 Find the smallest directory that you can delete while still freeing up enough space for the update.
 
 def space_hog(filename):
     dirs = defaultdict(int)
@@ -20,7 +21,9 @@ def space_hog(filename):
             case size, _:
                 for p in accumulate(curr):
                     dirs[p] += int(size)
-    print(sum(s for s in dirs.values() if s <= 100000))
+    print('The total sum of directories that are at most 100000bytes is: ',sum(s for s in dirs.values() if s <= 100000))
+    print('The smallest directory you can delete while still making enough space for the update is: ',min(s for s in dirs.values() if s >= dirs['/'] - 40000000))
+
 
 
 if len(sys.argv) == 2:
